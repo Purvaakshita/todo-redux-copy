4e2cd6f3eb001b68602a1c03681a13ce57098a67
nairx <nairxprv@gmail.com>
2025-04-08 13:38:10 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import Todo from "./features/todo/Todo";
 import User from "./features/user/User";
+import "./App.css"
 const App = () => {
-  return <User />;
+  return (
+    <div>
+      <div className="App-Header"><h1>My Application</h1></div>
+      <User />;
+      <div className="App-Footer">@Copyright 2025. All rights reserved.</div>
+    </div>
+  );
 };
 export default App;
